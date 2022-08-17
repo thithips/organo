f6b2c82c31bea29ecc2de453df5fd6c8ab35546e
Thiago Pereira Alves <thiagoalves@redejmu.local>
2022-08-17 09:27:35 -0300

    Pequenas alterações

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import './Banner.css'
 function Banner() {
     return (
         <header className="banner">
-             <img src="/images/banner.png" alt="Banner Principal"/>
+             <img src="/images/Banner.png" alt="Banner Principal"/>
         </header>
        
     )
